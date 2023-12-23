16802c39d8af305576a7da7c86660fa5f97199a3
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-12-23 20:17:56 +0530

    ok

--- src/main/java/com/example/Project06/Config/AppConfig.java
+++ src/main/java/com/example/Project06/Config/AppConfig.java
@@ -139,6 +139,8 @@ public class AppConfig {
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration config = new CorsConfiguration();
                 config.setAllowedOrigins(Collections.singletonList("https://preferable.co.in"));
+                config.setAllowedOrigins(Collections.singletonList("http://localhost:5400"));
+
                 config.setAllowedMethods(Collections.singletonList("*"));
                 config.setAllowCredentials(true);
                 config.setAllowedHeaders(Collections.singletonList("*"));
